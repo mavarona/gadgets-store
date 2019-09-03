842ed6d76d6f58a8984ce1ec310331d7b6c28d48
mavl <mvaronamac@gmail.coml>
2019-09-03 13:50:29 +0200

    Add pages to the project and router

--- src/App.js
+++ src/App.js
@@ -2,8 +2,30 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 
+import About from "./pages/AboutPage";
+import Cart from "./pages/CartPage";
+import Contact from "./pages/ContactPage";
+import Default from "./pages/Default";
+import Home from "./pages/HomePage";
+import Products from "./pages/ProductsPage";
+import SingleProduct from "./pages/SingleProductPage";
+
+import { Route, Switch } from "react-router-dom";
+
 function App() {
-  return <h1>App Component</h1>;
+  return (
+    <>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/product/:id" component={SingleProduct} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/ProductsPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductPage = () => {
+  return (
+    <>
+      <h1> Hello from ProductPage </h1>
+    </>
+  );
+};
+
+export default ProductPage;
