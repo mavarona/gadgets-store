a06cf99c053b19500adb6f4dbb9c3cf9425b0e8e
mavl <mvaronamac@gmail.coml>
2019-09-06 19:54:40 +0200

    Add Services in Home Page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,25 @@
 import React from "react";
 import Hero from "../components/Hero";
 import { Link } from "react-router-dom";
+import Services from "../components/HomePage/Services";
+import Featured from "../components/HomePage/Featured";
 
 const HomePage = () => {
   return (
     <>
       <Hero title="awesome gadgets" max="true">
-        <Link className="main-link" style={{ margin: "2rem" }} to="/products">
+        <Link
+          className="main-link"
+          style={{
+            margin: "2rem"
+          }}
+          to="/products"
+        >
           our products
         </Link>
       </Hero>
+      <Services />
+      <Featured />
     </>
   );
 };
