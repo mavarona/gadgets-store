1bc8943c1840c47a591984eea9b6a607ea3560b4
mavl <mvaronamac@gmail.coml>
2019-09-08 15:06:31 +0200

    Add single product page

--- src/context/context.js
+++ src/context/context.js
@@ -1,201 +1,196 @@
-import React, {
-    Component
-} from "react";
-import {
-    linkData
-} from "./linkData";
-import {
-    socialData
-} from "./socialData";
-import {
-    items
-} from "./productData";
+import React, { Component } from "react";
+import { linkData } from "./linkData";
+import { socialData } from "./socialData";
+import { items } from "./productData";
 
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
-    state = {
-        sidebarOpen: false,
-        cartOpen: false,
-        links: linkData,
-        socialIcons: socialData,
-        cart: [],
-        cartItems: 0,
-        cartSubTotal: 0,
-        cartTax: 0,
-        cartTotal: 0,
-        storeProducts: [],
-        filteredProducts: [],
-        featuredProducts: [],
-        singleProduct: {},
-        loading: true
-    };
+  state = {
+    sidebarOpen: false,
+    cartOpen: false,
+    links: linkData,
+    socialIcons: socialData,
+    cart: [],
+    cartItems: 0,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0,
+    storeProducts: [],
+    filteredProducts: [],
+    featuredProducts: [],
+    singleProduct: {},
+    loading: true
+  };
 
-    componentDidMount() {
-        this.setProducts(items);
+  componentDidMount() {
+    this.setProducts(items);
+  }
+
+  setProducts = products => {
+    let storeProducts = products.map(item => {
+      const { id } = item.sys;
+      const image = item.fields.image.fields.file.url;
+      const product = {
+        id,
+        ...item.fields,
+        image
+      };
+      return product;
+    });
+    let featuredProducts = storeProducts.filter(item => item.featured === true);
+    this.setState(
+      {
+        storeProducts,
+        filteredProducts: storeProducts,
+        featuredProducts,
+        cart: this.getStorageCart(),
+        singleProduct: this.getStorageProduct(),
+        loading: false
+      },
+      () => {
+        this.addTotals();
+      }
+    );
+  };
+
+  getStorageCart = () => {
+    let cart;
+    if (localStorage.getItem("cart")) {
+      cart = JSON.parse(localStorage.getItem("cart"));
+    } else {
+      cart = [];
     }
+    return cart;
+  };
 
-    setProducts = products => {
-        let storeProducts = products.map(item => {
-            const {
-                id
-            } = item.sys;
-            const image = item.fields.image.fields.file.url;
-            const product = {
-                id,
-                ...item.fields,
-                image
-            };
-            return product;
-        });
-        let featuredProducts = storeProducts.filter(item => item.featured === true);
-        this.setState({
-            storeProducts,
-            filteredProducts: storeProducts,
-            featuredProducts,
-            cart: this.getStorageCart(),
-            singleProduct: this.getStorageProduct(),
-            loading: false
-        }, () => {
-            this.addTotals()
-        });
+  getStorageProduct = () => {
+    return localStorage.getItem("singleProduct")
+      ? JSON.parse(localStorage.getItem("singleProduct"))
+      : {};
+  };
+
+  getTotals = () => {
+    let subTotal = 0;
+    let cartItems = 0;
+    this.state.cart.forEach(item => {
+      subTotal += item.total;
+      cartItems += item.count;
+    });
+    subTotal = parseFloat(subTotal.toFixed(2));
+    let tax = subTotal * 0.2;
+    tax = parseFloat(tax.toFixed(2));
+    let total = subTotal + tax;
+    total = parseFloat(total.toFixed(2));
+    return {
+      cartItems,
+      subTotal,
+      tax,
+      total
     };
+  };
 
-    getStorageCart = () => {
-        let cart;
-        if (localStorage.getItem('cart')) {
-            cart = JSON.parse(localStorage.getItem('cart'));
-        } else {
-            cart = [];
-        }
-        return cart;
-    };
+  addTotals = () => {
+    const totals = this.getTotals();
+    this.setState({
+      cartItems: totals.cartItems,
+      cartSubTotal: totals.subTotal,
+      cartTax: totals.tax,
+      cartTotal: totals.total
+    });
+  };
 
-    getStorageProduct = () => {
-        return localStorage.getItem('singleProduct') ? JSON.parse(localStorage.getItem('singleProduct')) : {};
-    };
+  syncStorage = () => {
+    localStorage.setItem("cart", JSON.stringify(this.state.cart));
+  };
 
-    getTotals = () => {
-        let subTotal = 0;
-        let cartItems = 0;
-        this.state.cart.forEach(item => {
-            subTotal += item.total;
-            cartItems += item.count;
-        });
-        subTotal = parseFloat(subTotal.toFixed(2));
-        let tax = subTotal * 0.2;
-        tax = parseFloat(tax.toFixed(2));
-        let total = subTotal + tax;
-        total = parseFloat(total.toFixed(2));
+  addToCart = id => {
+    let tmpCart = [...this.state.cart];
+    let tmpProducts = [...this.state.storeProducts];
+    let tmpItem = tmpCart.find(item => item.id === id);
+    if (!tmpItem) {
+      tmpItem = tmpProducts.find(item => item.id === id);
+      let total = tmpItem.price;
+      let cartItem = {
+        ...tmpItem,
+        count: 1,
+        total
+      };
+      tmpCart = [...tmpCart, cartItem];
+    } else {
+      tmpItem.count++;
+      tmpItem.total = tmpItem.price * tmpItem.count;
+      tmpItem.total = parseInt(tmpItem.total.toFixed(2));
+    }
+    this.setState(
+      () => {
         return {
-            cartItems,
-            subTotal,
-            tax,
-            total
-        }
-    };
+          cart: tmpCart
+        };
+      },
+      () => {
+        this.addTotals();
+        this.syncStorage();
+        this.openCart();
+      }
+    );
+  };
 
-    addTotals = () => {
-        const totals = this.getTotals();
-        this.setState({
-            cartItems: totals.cartItems,
-            cartSubTotal: totals.subTotal,
-            cartTax: totals.tax,
-            cartTotal: totals.total
-        });
-    };
+  setSingleProduct = id => {
+    let product = this.state.storeProducts.find(item => item.id === id);
+    localStorage.setItem("singleProduct", JSON.stringify(product));
+    this.setState({
+      singleProduct: {
+        ...product
+      },
+      loading: false
+    });
+  };
 
-    syncStorage = () => {
-        localStorage.setItem('cart', JSON.stringify(this.state.cart));
-    };
+  handleSidebar = () => {
+    this.setState({
+      sidebarOpen: !this.state.sidebarOpen
+    });
+  };
 
-    addToCart = id => {
-        let tmpCart = [...this.state.cart];
-        let tmpProducts = [...this.state.storeProducts];
-        let tmpItem = tmpCart.find(item => item.id === id);
-        if (!tmpItem) {
-            tmpItem = tmpProducts.find(item => item.id === id);
-            let total = tmpItem.price;
-            let cartItem = {
-                ...tmpItem,
-                count: 1,
-                total
-            };
-            tmpCart = [...tmpCart, cartItem];
-        } else {
-            tmpItem.count++;
-            tmpItem.total = tmpItem.price * tmpItem.count;
-            tmpItem.total = parseInt(tmpItem.total.toFixed(2));
-        }
-        this.setState(() => {
-            return {
-                cart: tmpCart
-            }
-        }, () => {
-            this.addTotals();
-            this.syncStorage();
-            this.openCart();
-        });
-    };
+  handleCart = () => {
+    this.setState({
+      cartOpen: !this.state.cartOpen
+    });
+  };
 
-    setSingleProduct = id => {
-        let product = this.state.storeProducts.find(item => item.id === id);
-        localStorage.setItem('singleProduct', JSON.stringify(product));
-        this.setState({
-            singleProduct: {
-                ...product
-            },
-            loading: false
-        })
-    };
+  closeCart = () => {
+    this.setState({
+      cartOpen: false
+    });
+  };
 
-    handleSidebar = () => {
-        this.setState({
-            sidebarOpen: !this.state.sidebarOpen
-        });
-    };
+  openCart = () => {
+    this.setState({
+      cartOpen: true
+    });
+  };
 
-    handleCart = () => {
-        this.setState({
-            cartOpen: !this.state.cartOpen
-        });
-    };
-
-    closeCart = () => {
-        this.setState({
-            cartOpen: false
-        });
-    };
-
-    openCart = () => {
-        this.setState({
-            cartOpen: true
-        });
-    };
-
-    render() {
-        return ( <
-            ProductContext.Provider value = {
-                {
-                    ...this.state,
-                        handleSidebar: this.handleSidebar,
-                        handleCart: this.handleCart,
-                        closeCart: this.closeCart,
-                        openCart: this.openCart,
-                        addToCart: this.addToCart,
-                        setSingleProduct: this.setSingleProduct
-                }
-            } > {
-                this.props.children
-            } <
-            /ProductContext.Provider>
-        );
-    }
+  render() {
+    return (
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleSidebar: this.handleSidebar,
+          handleCart: this.handleCart,
+          closeCart: this.closeCart,
+          openCart: this.openCart,
+          addToCart: this.addToCart,
+          setSingleProduct: this.setSingleProduct
+        }}
+      >
+        {" "}
+        {this.props.children}{" "}
+      </ProductContext.Provider>
+    );
+  }
 }
 
 const ProductConsumer = ProductContext.Consumer;
 
-export {
-    ProductProvider,
-    ProductConsumer
-};
+export { ProductProvider, ProductConsumer };
