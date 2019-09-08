008aea17b9ec30845b3ed1d5e62b9e7c724811f9
mavl <mvaronamac@gmail.coml>
2019-09-08 22:03:16 +0200

    fix

--- src/context/contentful.js
+++ src/context/contentful.js
@@ -1,6 +1,8 @@
-import * as contentful from 'contentful';
+import {
+    createClient
+} from 'contentful';
 
-export const client = contentful.createClient({
-    space: process.env.REACT_APP_SPACE_ID,
+export default createClient({
+    space: process.env.REACT_APP_API_SPACE,
     accessToken: process.env.REACT_APP_ACCESS_TOKEN
-})
+});
