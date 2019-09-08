8fc6ae1f41c556f9db9311edd104ac9ab1767740
mavl <mvaronamac@gmail.coml>
2019-09-08 16:35:10 +0200

    add sidecart component

--- src/components/Sidecart.js
+++ src/components/Sidecart.js
@@ -1,15 +1,50 @@
 import React from "react";
 import { ProductConsumer } from "../context";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Sidecart = () => {
   return (
     <ProductConsumer>
       {value => {
-        const { cartOpen, closeCart, cart } = value;
+        const { cartOpen, closeCart, cart, cartTotal } = value;
         return (
           <CartWrapper show={cartOpen} onClick={closeCart}>
-            <p>cart items</p>
+            <ul>
+              {cart.map(item => {
+                return (
+                  <li key={item.id} className="cart-item mb-4">
+                    <img
+                      src={`../${item.image}`}
+                      alt="cart item"
+                      width="35px"
+                    />
+                    <div className="mt-3">
+                      <h6 style={{ "text-transform": "uppercase" }}>
+                        {item.title}
+                      </h6>
+                      <h6
+                        className="text-title"
+                        style={{ "text-transform": "capitalize" }}
+                      >
+                        amount: {item.count}
+                      </h6>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+            <h4
+              className="text-main"
+              style={{ "text-transform": "capitalize" }}
+            >
+              cart total: ${cartTotal}
+            </h4>
+            <div className="my-5" style={{ "text-align": "center" }}>
+              <Link to="/cart" className="main-link">
+                cart page
+              </Link>
+            </div>
           </CartWrapper>
         );
       }}
@@ -30,6 +65,14 @@ const CartWrapper = styled.div`
   transform: ${props => (props.show ? "translateX(0)" : "translateX(100%)")};
   @media (min-width: 576px) {
     width: 20rem;
+  }
+  overflow: scroll;
+  padding: 2rem;
+  ul {
+    padding: 0 !important;
+  }
+  .cart-item {
+    list-style-type: none;
   }
 `;
 
