1dce5588c6bc6e05ea5e92ccf9f020dd70e4986c
mavl <mvaronamac@gmail.coml>
2019-09-08 22:05:15 +0200

    fix

--- src/context/contentful.js
+++ src/context/contentful.js
@@ -1,8 +1,8 @@
-import {
-    createClient
-} from 'contentful';
+import * as contentful from "contentful";
 
-export default createClient({
-    space: process.env.REACT_APP_API_SPACE,
+export const client = contentful.createClient({
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: process.env.REACT_APP_SPACE_ID,
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
     accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
