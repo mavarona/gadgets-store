5d0cd30adc5e93e0b9bd4bf8b700e94b5e2160b2
mavl <mvaronamac@gmail.coml>
2019-09-08 19:32:01 +0200

    add logic search

--- src/context/context.js
+++ src/context/context.js
@@ -20,7 +20,13 @@ class ProductProvider extends Component {
     filteredProducts: [],
     featuredProducts: [],
     singleProduct: {},
-    loading: true
+    loading: true,
+    search: "",
+    price: 0,
+    min: 0,
+    max: 0,
+    company: "all",
+    shipping: false
   };
 
   componentDidMount() {
@@ -39,6 +45,8 @@ class ProductProvider extends Component {
       return product;
     });
     let featuredProducts = storeProducts.filter(item => item.featured === true);
+    let maxPrice = Math.max(...storeProducts.map(item=>item.price));
+    console.log(maxPrice);
     this.setState(
       {
         storeProducts,
@@ -46,7 +54,9 @@ class ProductProvider extends Component {
         featuredProducts,
         cart: this.getStorageCart(),
         singleProduct: this.getStorageProduct(),
-        loading: false
+        loading: false,
+        price: maxPrice,
+        max: maxPrice
       },
       () => {
         this.addTotals();
@@ -245,6 +255,43 @@ class ProductProvider extends Component {
     );
   };
 
+  handleChange = e => {
+      const name = e.target.name;
+      const value = e.target.type === 'checkbox'? e.target.checked: e.target.value;
+      this.setState({
+        [name]:value
+      }, this.sortData);
+  };
+
+  sortData = () =>{
+    const { storeProducts, price, company, shipping, search } = this.state;
+
+    let tempPrice = parseInt(price);
+
+    let tempProducts = [...storeProducts];
+    // filtering based on price
+    tempProducts = tempProducts.filter(item => item.price <= tempPrice);
+    // filtering based on company
+    if (company !== "all") {
+      tempProducts = tempProducts.filter(item => item.company === company);
+    }
+    if (shipping) {
+      tempProducts = tempProducts.filter(item => item.freeShipping === true);
+    }
+    if (search.length > 0) {
+      tempProducts = tempProducts.filter(item => {
+        let tempSearch = search.toLowerCase();
+        let tempTitle = item.title.toLowerCase().slice(0, search.length);
+        if (tempSearch === tempTitle) {
+          return item;
+        }
+      });
+    }
+    this.setState({
+      filteredProducts: tempProducts
+    });
+  }
+
   render() {
     return (
       <ProductContext.Provider
@@ -259,7 +306,8 @@ class ProductProvider extends Component {
           increment: this.increment,
           decrement: this.decrement,
           removeItem: this.removeItem,
-          clearCart: this.clearCart
+          clearCart: this.clearCart,
+          handleChange: this.handleChange
         }}
       >
         {this.props.children}
